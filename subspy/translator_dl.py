0a352a681408cbe75a9a2fd4cfbedc20f26817f2
SayCV <sssxyc@163.com>
2023-04-11 06:48:28 +0800

    fix text length for dl

--- subspy/translator_dl.py
+++ subspy/translator_dl.py
@@ -11,6 +11,7 @@ class DeepLearningTranslator:
     # device
     #   "auto" "cpu" or "gpu"
     def __init__(self, model="m2m100", device="auto"):
+        self.limit_of_length = 200
         self.device = device
         self.model = model
         self.mt = dlt.TranslationModel(model_or_path=model, device=device)
@@ -32,7 +33,7 @@ class DeepLearningTranslator:
         translated_sents = self.mt.translate(sents, source, target, batch_size=32, verbose=True)
         print(' '.join(translated_sents))
 
-    def translate(self, text_list: list, translator: str, from_language: str, to_language: str) -> str:
+    def _translate(self, text_list: list, translator: str, from_language: str, to_language: str) -> str:
         source = from_language
         target = to_language
         if from_language == 'chs':
@@ -45,6 +46,29 @@ class DeepLearningTranslator:
             target = dlt.lang.ENGLISH
         result = self.mt.translate(text_list, source, target, batch_size=32, verbose=True)
         return '\n'.join(result)
+
+    def translate(self, text_list: list, translator: str, from_language: str, to_language: str) -> str:
+        result = self.translate_lines(
+            text_list, translator, from_language, to_language)
+        return result
+
+    def translate_lines(self, text_list: list, translator: str, from_language: str, to_language: str) -> str:
+        translated = ''
+        last_idx = 0
+        total_length = 0
+        for i in range(len(text_list)):
+            total_length += len(text_list[i])
+            if total_length > self.limit_of_length:
+                translated += self._translate(
+                    text_list[last_idx:i], translator, from_language, to_language)
+                translated += '\n'
+                time.sleep(1)
+                last_idx = i
+                total_length = 0
+        translated += self._translate(
+            text_list[last_idx:], translator, from_language, to_language)
+
+        return translated
 
 
 if __name__ == "__main__":
